ea5cc2587f35b85e038a82c18af6d029e21a99bb
asd12288 <ilanchelly@gmail.com>
2025-01-13 11:55:14 +0200

    date

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,30 +1,34 @@
-import { useNavigate } from "react-router-dom";
-import "../styles/styles.css"; // or "./Navbar.css" if you keep it separate
-import accountImage from "../assets/account.png";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSignOutAlt } from "@fortawesome/free-solid-svg-icons";
+import { useNavigate } from 'react-router-dom';
+import '../styles/styles.css'; // or "./Navbar.css" if you keep it separate
+import accountImage from '../assets/account.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
 
 function Navbar() {
   const navigate = useNavigate();
 
   const handleLogout = () => {
-    localStorage.removeItem("user");
-    navigate("/login");
+    localStorage.removeItem('user');
+    navigate('/login');
   };
 
   return (
     <header className="header">
       <h1>
-        Welcome Back{" "}
-        {localStorage.getItem("user").charAt(0).toUpperCase() +
-          localStorage.getItem("user").slice(1) || "user"}
+        Welcome Back{' '}
+        {localStorage.getItem('user').charAt(0).toUpperCase() +
+          localStorage.getItem('user').slice(1) || 'user'}
         !
       </h1>
+      <p>
+        The data refrash every 8 Hours, latest update:{' '}
+        {Date.now().toLocaleString()}
+      </p>
 
       <div className="header-right">
         <img className="icon-account" src={accountImage} alt="account" />
         <span className="username">
-          {localStorage.getItem("user").toUpperCase() || "user"}
+          {localStorage.getItem('user').toUpperCase() || 'user'}
         </span>
         <button className="logout" onClick={handleLogout}>
           <FontAwesomeIcon icon={faSignOutAlt} /> Logout
